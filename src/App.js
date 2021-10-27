6ae7e3a4d2c64d9be4ac92f30f160427a90c2c9d
netajik <kancharapunetaji@gmail.com>
2021-10-27 22:00:40 +0530

    basic voice test  command added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
 
+const alanKey =
+  "31b38be55ce59b7b039514536387cf222e956eca572e1d8b807a3e2338fdd0dc/stage";
 const App = () => {
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command }) => {
+        if (command === "testCommand") {
+          alert("Hello World");
+        }
+      },
+      rootEl: document.getElementById("alan-btn"),
+    });
+  }, []);
   return <div>Hello World</div>;
 };
 
